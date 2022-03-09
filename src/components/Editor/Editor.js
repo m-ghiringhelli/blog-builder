750715016f9d43ef80baab916a78f63d631b07b2
m-ghiringhelli <mghiring@gmail.com>
2022-03-09 14:29:56 -0800

    title displays when type

--- src/components/Editor/Editor.js
+++ src/components/Editor/Editor.js
@@ -2,15 +2,20 @@ import React from 'react';
 
 import './Editor.css';
 
-export default function Editor() {
+export default function Editor({
+  title, 
+  setTitle,
+  subtitle,
+  setSubtitle 
+}) {
   return (
     <div className="editor">
       <div className="form-control">
-        <input name="title" type="text" />
+        <input name="title" type="text" value={title} onChange={(e) => setTitle(e.target.value)}/>
         <label htmlFor="title">Title</label>
       </div>
       <div className="form-control">
-        <input type="text" />
+        <input type="text" value={subtitle}/>
         <label>Subtitle</label>
       </div>
       <div className="form-control">
